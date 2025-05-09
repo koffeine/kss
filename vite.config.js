1dfb814d23d67a994cbb0fc577910f4d7e80ed2e
Kornél Horváth <kornel.horvath@me.com>
2025-05-09 17:52:27 +0200

    Use type import instead of normal import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig(({ mode }) => ({
+/** @type {import('vite').UserConfigExport} */
+export default ({ mode }) => ({
 	base: mode === 'production' ? '/kss/' : '/',
 	server: { open: true }
-}));
+});
